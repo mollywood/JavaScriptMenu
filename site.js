2751ab3b2b9f1fb2d9b0670c6a360b99e1a8df44
Molly Wood <mollywood@Interzone.localdomain>
2018-05-24 11:23:09 -0500

    menu upload

---
+++ site.js
@@ -0,0 +1,41 @@
+let dishesDiv = document.querySelector("#dishesDiv")
+
+let btnAll = document.querySelector("#btnAll")
+btnAll.addEventListener('click',function(){
+  populateDishes(dishes)
+})
+
+let btnStarters = document.querySelector('#btnStarters')
+btnStarters.addEventListener('click', function(){
+  let filteredDishes = getDishesBy("Starters")
+})
+
+let btnEntrees = document.querySelector('#btnEntrees')
+btnEntrees.addEventListener('click', function(){
+  let filteredDishes = getDishesBy("Entrees")
+})
+
+let btnDesserts = document.querySelector('#btnDesserts')
+btnDesserts.addEventListener('click', function(){
+  let filteredDishes = getDishesBy("Desserts")
+})
+
+function getDishesBy(course) {
+  let filteredDishes = dishes.filter(function(dish) {
+    return dishes.course == course
+  })
+  return filteredDishes
+}
+
+function populateDishes(dishesToDisplay) {
+  dishesDiv.innerHTML = ''
+
+  for(let index = 0; index < dishes.length; index++) {
+
+    let dishItem = `<img src='${dishes[index].imageURL}' />
+    <h4>${dishes[index].title}</h4>
+    <p> ${dishes[index].description} </p>`
+
+    dishesDiv.innerHTML += dishItem
+  }
+}
